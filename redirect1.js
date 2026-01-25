fd195f84e35764b9f8a94e09a87ee5feb6c2706e
101 <jlyanko76@googlemail.com>
2026-01-25 20:32:37 +0300

    Delete redirect1.js

--- redirect1.js
+++
@@ -1,8 +0,0 @@
-// Function to navigate to 'Patience.html'
-function goToPage() {
-    // Change the current page's URL to 'Patience.html'
-    window.location.href = "construct1.github.io/Be%20Patient.html";
-
-}
-
-
