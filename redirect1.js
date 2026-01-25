c361ee0a9003d9bc9f377f6a52a6a415f0bff274
101 <jlyanko76@googlemail.com>
2026-01-25 20:28:49 +0300

    Update redirect1.js

--- redirect1.js
+++ redirect1.js
@@ -1,7 +1,8 @@
 // Function to navigate to 'Patience.html'
 function goToPage() {
     // Change the current page's URL to 'Patience.html'
-    window.location.href = "Be%20Patient.html";
+    window.location.href = "construct1.github.io/Be%20Patient.html";
 
 }
+
 
