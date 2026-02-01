4a09044aa501573947b536e723affd514223b632
101 <jlyanko76@googlemail.com>
2026-02-01 11:07:37 +0300

    Update redirectthing.js

--- redirectthing.js
+++ redirectthing.js
@@ -1,9 +1,10 @@
 // Function to navigate to 'Patience.html'
 function goToPage() {
     // Change the current page's URL to 'Patience.html'
-    window.location.href = "Be%20Patient.html";
+    window.location.href = "nosy.html";
 
 }
+
 
 
 
